06dc89f0dccae5e673bf49603d512f94bcff556b
MafeOssa <mafe201406@gmail.com>
2021-06-17 21:47:38 -0500

    Nuevos ajustes de la app

--- stomp-shoes/src/App.js
+++ stomp-shoes/src/App.js
@@ -1,41 +1,22 @@
-// import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 import BarraNavegar from './components/BarraNavegar';
-// import Children from './components/Children';
-// import State from './components/State';
-// import UseState from './components/UseState';
-import ItemCount from './components/ItemCount';
-import Item from './components/Item';
+import ItemListContainer from './components/ItemListContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <BarraNavegar/>
-      <ItemCount/>
-      <Item/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <Children>
-        <h1>Hola React</h1>
-      </Children>
-      <State/>
-      <UseState/> */}
-      <ItemCount/>
-    </div>
-  );
-}
+let name = "Bienvenido a Stomp Shoes"
+  class App extends Component {
+    render() {
+      return(
+        <Fragment>
+          <BarraNavegar/>
+          <ItemListContainer/>
+          <h2 className="App">Hola <span>{name}</span></h2>
+          {/* <Children>           
+            <h1>Hola React</h1>        
+          </Children> */}
+        </Fragment>
+      )
+    }
+  }
 
 export default App;
--- stomp-shoes/src/components/CartWidget.js
+++ stomp-shoes/src/components/CartWidget.js
@@ -4,8 +4,8 @@ import React from 'react';
 function CartWidget() {
     return (
         <div className="producto">
-        <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a>
-    </div>
+            <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a>
+        </div>
     )
 }
 
